4085f8bdcb8719a062c17f03202d351d077a824c
Ezy <ezra@passionfruit.co.nz>
2020-01-29 20:47:29 +1300

    fix result props tests

--- src/components/Result.test.js
+++ src/components/Result.test.js
@@ -8,16 +8,16 @@ Enzyme.configure({ adapter: new Adapter() });
 const { shallow } = Enzyme;
 
 it('Should render the Result component with message about current turn', () => {
-  const wrapper = shallow(<Result turn={'o'} draw={false} />);
-  expect(wrapper.find('p').node.props.children).toEqual('It\'s O\'s turn.');
+  const wrapper = shallow(<Result turn={'Banking'} draw={false} />);  
+  expect(wrapper.getElement().props.children).toEqual(<p>It's BANKING's turn.</p>);
 });
 
 it('Should render the Result component with message about winning symbol', () => {
-  const wrapper = shallow(<Result won={'x'} draw={false} turn={'x'} />);
-  expect(wrapper.find('p').node.props.children).toEqual('Yay! X won!');
+  const wrapper = shallow(<Result won={'Crypto'} draw={false} turn={'Crypto'} />);
+  expect(wrapper.getElement().props.children).toEqual(<p>Yay! CRYPTO won!</p>);
 });
 
 it('Should render the Result component with message about the draw', () => {
-  const wrapper = shallow(<Result draw={true} turn={'x'} />);
-  expect(wrapper.find('p').node.props.children).toEqual('We have a draw!');
+  const wrapper = shallow(<Result draw={true} turn={'Crypto'} />);
+  expect(wrapper.getElement().props.children).toEqual(<p>We have a draw!</p>);
 });
